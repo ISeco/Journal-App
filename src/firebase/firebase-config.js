4aa8aa2e140dce6e661f0b9df98b6f4b20985f24
Ignacio Seco <ignacio.seco@inacapmail.cl>
2022-05-28 19:37:58 -0400

    finish app

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,33 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const {
+    API_KEY,
+    AUTH_DOMAIN,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGING_SENDER_ID,
+    APP_ID,
+} = process.env
+
+const firebaseConfig = {
+    apiKey: `${API_KEY}`,
+    authDomain: `${AUTH_DOMAIN}`,
+    projectId: `${PROJECT_ID}`,
+    storageBucket: `${STORAGE_BUCKET}`,
+    messagingSenderId: `${MESSAGING_SENDER_ID}`,
+    appId: `${APP_ID}`
+};
+  
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+
+export {
+    db,
+    googleAuthProvider,
+    firebase
+}
